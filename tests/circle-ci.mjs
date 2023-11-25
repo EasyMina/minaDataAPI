ae9aed374f5e64dbfb41c8615057d67d2c19d93f
αηdr3α5 ɮαηɦօlʐ3ʀ <AndreasBanholzer@Me.com>
2023-11-25 10:55:36 +0100

    setup test

--- tests/circle-ci.mjs
+++ tests/circle-ci.mjs
@@ -3,16 +3,17 @@ import { MinaServer } from '../src/MinaServer.mjs'
 try {
     const minaServer = new MinaServer()
     const version = 'v0.0.2'
+    const url = 'https://...'
     const schema = minaServer.getOpenAiSchema( { 
         'title': 'Learn Mina Blockchain API',
         'description': `The Learn Mina Blockchain API provides tools for querying the Mina blockchain, supporting operations such as retrieving account balances and transaction details, specifically for the 'berkeley' network. It allows for precise queries with various parameters, including public keys and block heights, and is accessible via ${url}.`,
         'version': 'v0.0.2',
-        'url': '...'
+        url
     } )
     console.log( 'Success!' )
     process.exit( 0 )
 } catch( e ) {
-    console.log( 'Error!' )
+    console.log( `Error! ${e}` )
     process.exit( 1 )
 }
 
